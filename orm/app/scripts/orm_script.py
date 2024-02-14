2eb9db018f8c004903262553771291dfd060636b
Aboobacker Sidheeque <sidhub41214@gmail.com>
2024-02-14 16:29:14 +0530

    updated

--- orm/app/scripts/orm_script.py
+++ orm/app/scripts/orm_script.py
@@ -5,7 +5,7 @@ from django.db import connection
 from pprint import pprint
 
 def run():
-    
+    pass
     
     # staff, created = Staff.objects.get_or_create(name='John Wick')
     # res = Restaurant.objects.first()
